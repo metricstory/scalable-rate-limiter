8c88044f8a690c5453aa98b05d871621e71047d1
Will Schumacher <meintodengel@gmail.com>
2016-12-28 15:04:43 -0800

    add tests for actual rate limiting

--- test/ratelimiter.js
+++ test/ratelimiter.js
@@ -15,4 +15,31 @@ describe('RateLimiter', () => {
       expect(rl.enableLogging).to.equal(true)
     })
   })
+
+  describe('.rateLimitFunction without daily limit', () => {
+    const rl = new RateLimiter(client);
+
+    it('will call the supplied callback', done => {
+      const callback = (limited) => {
+        expect(limited).to.equal(false);
+        done();
+      }
+      rl.rateLimitFunction('user', callback)
+    })
+
+    it('will ratelimit for at least enough time', done => {
+      const callback = (limited) => {
+        const after = Date.now();
+        expect(limited).to.equal(false);
+        expect(after-before).to.be.above(1000)
+        expect(after-before).to.be.below(2000)
+        done()
+      }
+      const before = Date.now();
+      for (var i = 0; i < 10; i++) {
+        rl.rateLimitFunction('user', o => {return o});
+      }
+      rl.rateLimitFunction('user', callback);
+    })
+  })
 })
